ae8878e6632a0b16a1326e0f1a3f089c45bc4743
malegebide123 <1009473166@qq.com>
2017-03-22 14:03:24 +0800

    activity

---
+++ IChat/app/src/main/java/com/example/asus/ichat/act/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.asus.ichat.act;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 残殇 ASUS on 2017/3/22.
+ * 站在巅峰，看世界。
+ * 落在深渊，思人生。
+ * 年轻没有失败，
+ * 自信创造未来！
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
